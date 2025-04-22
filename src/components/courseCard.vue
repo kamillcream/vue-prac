01583f7d474b3f035c62b7f23b2084d5779d269b
Jinyoung <glitt5384@naver.com>
2025-04-22 22:51:37 +0900

    FEAT: 코스 정보 시각화하는 카드 컴포넌트

---
+++ src/components/courseCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  course: Object
+})
+</script>
+
+<template>
+  <div class="course-card">
+    <h3>코스</h3>
+    <ul>
+      <li v-for="(place, i) in course.places" :key="i">
+        {{ place.name }} - {{ place.time }}
+      </li>
+    </ul>
+  </div>
+</template>
