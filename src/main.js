911d5eb164427fc90da073913e7f976bb8d4c744
Jinyoung <glitt5384@naver.com>
2025-04-27 21:56:00 +0900

    FEAT: app.use(store) 추가

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './routers/router.js'
+import store from './store.js'
 
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.mount('#app')
